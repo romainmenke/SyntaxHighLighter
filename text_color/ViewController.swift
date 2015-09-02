bfcc3e818b4b9ba0bae3e2b47debcdfd3915903a
Romain Menke <romainmenke@me.com>
2015-09-02 20:25:37 +0200

    Initial Commit

---
+++ text_color/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  text_color
+//
+//  Created by Romain Menke on 02/09/15.
+//  Copyright © 2015 menke dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
